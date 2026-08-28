67252a7b7d70f651ec452359de73f50b2079d72e
voidVoy7 <voidvoy7@proton.me>
2026-08-28 04:08:20 +0200

    Added Terra repo back in

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,7 +17,8 @@ cp -avf "/ctx/system_files"/. /
 # Microsoft
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
-
+# Terra
+sed -i '/^\[terra\]$/,/^\[/{s/^enabled=0$/enabled=1/}' /etc/yum.repos.d/terra.repo
 
 dnf5 install -y tmux
 dnf install -y helix
