2b2839b5c8ea89d3e3ed4537e13f9ce8c9030042
voidVoy7 <voidvoy7@proton.me>
2026-08-28 02:37:52 +0200

    Added packages I want

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,26 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
+# Repos
+# Terra repo
+dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+# Microsoft
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
+
 dnf5 install -y tmux
+dnf install -y helix
+dnf install -y gimp
+dnf install -y fastfetch
+dnf install -y ghostty
+dnf install -y vesktop
+dnf install -y code
+
+# Steam installation
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
+dnf5 in -y --setopt=install_weak_deps=False steam
+# End of Steam installation
+
 
 # Use a COPR Example:
 #
